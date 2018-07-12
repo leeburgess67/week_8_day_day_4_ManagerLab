675d7bd14101517fe66af075f0ff23016a41f302
leeburgess67 <leeburgessmail@gmail.com>
2018-07-12 11:55:13 +0100

    sets up Manager class

--- src/main/java/models/Manager.java
+++ src/main/java/models/Manager.java
@@ -1,4 +1,50 @@
 package models;
 
-public class Manager {
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "managers")
+public class Manager extends Employee {
+
+    private int budget;
+    private String department;
+    private List<Administrator> administrators;
+
+    public Manager() {}
+
+    public Manager(String name, String niNo, int salary, int budget, String department) {
+        super(name, niNo, salary);
+        this.budget = budget;
+        this.department = department;
+        this.administrators = new ArrayList<Administrator>();
+    }
+
+    @Column(name = "budget")
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+
+    @Column(name = "department")
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @OneToMany(mappedBy = "manager", fetch = FetchType.LAZY)
+    public List<Administrator> getAdministrators() {
+        return administrators;
+    }
+
+    public void setAdministrators(List<Administrator> administrators) {
+        this.administrators = administrators;
+    }
 }
