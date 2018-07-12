3e2cbeab71cee2fe7a432d29a79498bbff508adc
leeburgess67 <leeburgessmail@gmail.com>
2018-07-12 12:01:21 +0100

    sets up Administrator class

--- src/main/java/models/Administrator.java
+++ src/main/java/models/Administrator.java
@@ -1,4 +1,30 @@
 package models;
 
-public class Administrator {
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "administrators")
+public class Administrator extends Employee {
+
+    private Manager manager;
+
+    public Administrator() {}
+
+    public Administrator(String name, String niNo, int salary, Manager manager) {
+        super(name, niNo, salary);
+        this.manager = manager;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "manager_id", nullable = false)
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
 }
